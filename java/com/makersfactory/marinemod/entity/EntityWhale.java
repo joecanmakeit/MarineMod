0bd1ea133a8c40abec6032d6936517aed5925bf4
jkang8 <john00kang@gmail.com>
2014-11-08 19:47:30 -0800

    Added whale water movement and AI

--- java/com/makersfactory/marinemod/entity/EntityWhale.java
+++ java/com/makersfactory/marinemod/entity/EntityWhale.java
@@ -2,34 +2,54 @@ package com.makersfactory.marinemod.entity;
 
 import java.util.Random;
 
+
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIFollowParent;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMate;
 import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
-import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAITempt;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityBat;
 import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.entity.passive.EntitySquid;
 import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.passive.EntityWaterMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeModContainer;
 
-public class EntityWhale extends EntityAnimal {
+public class EntityWhale extends EntityWaterMob {
 	
 	Random random = new Random();
 	
 	public EntityWhale(World par1World) {
 		super(par1World);
-
+		this.isImmuneToFire = true;
+		
+		this.tasks.addTask(1, new EntityAISwimming(this));
+		this.tasks.addTask(2, new EntityAIPanic(this, 1.4D));
+		//this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+		this.tasks.addTask(3, new EntityAITempt(this, 1.0D, Items.fish, false));
+		//this.tasks.addTask(4, new EntityAIFollowParent(this, 1.1D));
+		this.tasks.addTask(0, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(4, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(5, new EntityAILookIdle(this));
+        
 	}
     
     public void onUpdate()
@@ -46,10 +66,10 @@ public class EntityWhale extends EntityAnimal {
     {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(12.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.01D);
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(5.0D);
     }
 
-	@Override
+	/*@Override
 	public EntityAgeable createChild(EntityAgeable p_90011_1_) {
 		// TODO Auto-generated method stub
 		return null;
@@ -57,7 +77,7 @@ public class EntityWhale extends EntityAnimal {
     {
         return "mob.pig.say";
     }
-
+*/
      // Returns the sound this mob makes when it is hurt.
     protected String getHurtSound()
     {
