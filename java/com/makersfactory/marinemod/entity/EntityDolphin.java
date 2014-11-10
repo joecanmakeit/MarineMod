5b363db9b046d88e281e4604716f7ffbf059dd2f
Joe Wise <JosephWise93@gmail.com>
2014-11-09 16:43:28 -0800

    added dolphin AI and RenderDolphin

--- java/com/makersfactory/marinemod/entity/EntityDolphin.java
+++ java/com/makersfactory/marinemod/entity/EntityDolphin.java
@@ -1,13 +1,29 @@
 package com.makersfactory.marinemod.entity;
 
+import net.minecraft.entity.ai.EntityAIFollowParent;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMate;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAITempt;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.passive.EntityWaterMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.world.World;
 
 public class EntityDolphin extends EntityWaterMob {
 
 	public EntityDolphin(World p_i1695_1_) {
 		super(p_i1695_1_);
-		// TODO Auto-generated constructor stub
+		this.setSize(1.0F,1.0F);
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(1, new EntityAIPanic(this, 1.4D));
+		this.tasks.addTask(2,  new EntityAITempt(this, 1.0D, Items.fish, false));
+		this.tasks.addTask(3, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(4, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(5, new EntityAILookIdle(this));
 	}
 
     public boolean isAIEnabled()
---
+++ java/com/makersfactory/marinemod/renderer/RenderDolphin.java
@@ -0,0 +1,76 @@
+package com.makersfactory.marinemod.renderer;
+
+import org.lwjgl.opengl.GL11;
+
+import com.makersfactory.marinemod.entity.EntityDolphin;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderDolphin extends RenderLiving {
+
+    private static final ResourceLocation dolphinTextures = new ResourceLocation("myassets:textures/entity/jellyfishtest.png");
+    private ModelBase scaleAmount;
+    //private static final String __OBFID = "CL_00001024";
+
+    public RenderDolphin(ModelBase par1ModelBase, ModelBase par2ModelBase, float par3)
+    {
+        super(par1ModelBase, par3);
+        this.scaleAmount = par2ModelBase;
+    }
+    
+    protected int shouldRenderPass(EntityDolphin par1EntitySlime, int par2, float par3)
+    {
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        return -1;
+    }
+
+    /**
+     * Allows the render to do any OpenGL state modifications necessary before the model is rendered. Args:
+     * entityLiving, partialTickTime
+     */
+    protected void preRenderCallback(EntityDolphin par1EntitySlime, float par2)
+    {
+        GL11.glEnable(GL11.GL_NORMALIZE);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+    }
+
+    /**
+     * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+     */
+    protected ResourceLocation getEntityTexture(EntityDolphin par1EntitySlime)
+    {
+        return dolphinTextures;
+    }
+
+    /**
+     * Allows the render to do any OpenGL state modifications necessary before the model is rendered. Args:
+     * entityLiving, partialTickTime
+     */
+    protected void preRenderCallback(EntityLivingBase par1EntityLivingBase, float par2)
+    {
+        this.preRenderCallback((EntityDolphin)par1EntityLivingBase, par2);
+    }
+
+    /**
+     * Queries whether should render the specified pass or not.
+     */
+    protected int shouldRenderPass(EntityLivingBase par1EntityLivingBase, int par2, float par3)
+    {
+        return this.shouldRenderPass((EntityDolphin)par1EntityLivingBase, par2, par3);
+    }
+
+    /**
+     * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+     */
+    protected ResourceLocation getEntityTexture(Entity par1Entity)
+    {
+        return this.getEntityTexture((EntityDolphin)par1Entity);
+    }
+
+}
