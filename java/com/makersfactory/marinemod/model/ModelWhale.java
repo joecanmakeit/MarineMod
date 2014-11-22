aa17288a3b3d385df4678bca91d08e88feb329ba
jkang8 <john00kang@gmail.com>
2014-11-22 01:06:39 -0800

    Reskinned and remodeled gray whale

--- java/com/makersfactory/marinemod/model/ModelWhale.java
+++ java/com/makersfactory/marinemod/model/ModelWhale.java
@@ -11,28 +11,29 @@ import net.minecraft.util.MathHelper;
 public class ModelWhale extends ModelBase
 {
   //fields
-    ModelRenderer head;
+    ModelRenderer neck;
     ModelRenderer body1;
     ModelRenderer leftpectoralfin;
     ModelRenderer rightpectoralfin;
-    ModelRenderer jaw;
     ModelRenderer pectoralfinr;
     ModelRenderer body3;
     ModelRenderer body4;
-    ModelRenderer lefttail;
-    ModelRenderer righttail;
+    ModelRenderer tail;
+    ModelRenderer body6;
+    ModelRenderer body5;
+    ModelRenderer head;
   
   public ModelWhale()
   {
     textureWidth = 64;
     textureHeight = 32;
     
-      head = new ModelRenderer(this, 0, 0);
-      head.addBox(-4F, -4F, -8F, 8, 6, 8);
-      head.setRotationPoint(0F, 14F, -6F);
-      head.setTextureSize(64, 32);
-      head.mirror = true;
-      setRotation(head, 0F, 0F, 0F);
+      neck = new ModelRenderer(this, 0, 0);
+      neck.addBox(-3F, -3F, -9F, 8, 7, 9);
+      neck.setRotationPoint(-1F, 14F, -7F);
+      neck.setTextureSize(64, 32);
+      neck.mirror = true;
+      setRotation(neck, 0F, 0F, 0F);
       body1 = new ModelRenderer(this, 28, 8);
       body1.addBox(-5F, -10F, -7F, 10, 14, 8);
       body1.setRotationPoint(0F, 11F, 3F);
@@ -40,69 +41,76 @@ public class ModelWhale extends ModelBase
       body1.mirror = true;
       setRotation(body1, 1.570796F, -0.0174533F, 0F);
       leftpectoralfin = new ModelRenderer(this, 28, 8);
-      leftpectoralfin.addBox(0F, 2F, -1F, 8, 3, 1);
+      leftpectoralfin.addBox(0F, 0F, -1F, 8, 3, 1);
       leftpectoralfin.setRotationPoint(5F, 16F, -6F);
       leftpectoralfin.setTextureSize(64, 32);
       leftpectoralfin.mirror = true;
       setRotation(leftpectoralfin, 1.797689F, 0F, 0F);
       rightpectoralfin = new ModelRenderer(this, 0, 0);
-      rightpectoralfin.addBox(2F, 21F, 0F, 10, 7, 7);
-      rightpectoralfin.setRotationPoint(-7F, -11F, 7F);
+      rightpectoralfin.addBox(-5F, -3F, 0F, 10, 7, 10);
+      rightpectoralfin.setRotationPoint(0F, 14F, 7F);
       rightpectoralfin.setTextureSize(64, 32);
       rightpectoralfin.mirror = true;
       setRotation(rightpectoralfin, 0F, 0F, 0F);
-      jaw = new ModelRenderer(this, 0, 0);
-      jaw.addBox(0F, 0F, 0F, 8, 1, 8);
-      jaw.setRotationPoint(-4F, 17F, -14F);
-      jaw.setTextureSize(64, 32);
-      jaw.mirror = true;
-      setRotation(jaw, 0.1396263F, 0F, 0F);
       pectoralfinr = new ModelRenderer(this, 28, 8);
       pectoralfinr.addBox(0F, 0F, 0F, 8, 3, 1);
-      pectoralfinr.setRotationPoint(-4F, 15F, -5F);
+      pectoralfinr.setRotationPoint(-4F, 16F, -5F);
       pectoralfinr.setTextureSize(64, 32);
       pectoralfinr.mirror = true;
       setRotation(pectoralfinr, -1.797689F, 3.141593F, 0F);
       body3 = new ModelRenderer(this, 0, 0);
-      body3.addBox(-5F, 10F, 14F, 10, 5, 5);
-      body3.setRotationPoint(0F, 0F, 0F);
+      body3.addBox(-4F, -3F, 0F, 8, 6, 9);
+      body3.setRotationPoint(0F, 15F, 17F);
       body3.setTextureSize(64, 32);
       body3.mirror = true;
       setRotation(body3, 0F, 0F, 0F);
       body4 = new ModelRenderer(this, 0, 0);
-      body4.addBox(0F, 0F, 0F, 10, 4, 5);
-      body4.setRotationPoint(-5F, 10F, 19F);
+      body4.addBox(-3F, -3F, 0F, 6, 5, 8);
+      body4.setRotationPoint(0F, 16F, 26F);
       body4.setTextureSize(64, 32);
       body4.mirror = true;
       setRotation(body4, 0F, 0F, 0F);
-      lefttail = new ModelRenderer(this, 0, 0);
-      lefttail.addBox(0F, 0F, 0F, 8, 2, 3);
-      lefttail.setRotationPoint(1F, 10F, 23F);
-      lefttail.setTextureSize(64, 32);
-      lefttail.mirror = true;
-      setRotation(lefttail, 0.2359809F, 0F, 0F);
-      righttail = new ModelRenderer(this, 0, 0);
-      righttail.addBox(0F, 0F, 0F, 8, 2, 3);
-      righttail.setRotationPoint(-1F, 12F, 23F);
-      righttail.setTextureSize(64, 32);
-      righttail.mirror = true;
-      setRotation(righttail, 2.844164F, 3.141593F, 0F);
+      tail = new ModelRenderer(this, 0, 0);
+      tail.addBox(-6F, 0F, 0F, 12, 2, 4);
+      tail.setRotationPoint(0F, 16F, 45F);
+      tail.setTextureSize(64, 32);
+      tail.mirror = true;
+      setRotation(tail, -0.1047198F, 0F, 0F);
+      body6 = new ModelRenderer(this, 0, 0);
+      body6.addBox(-2F, -2F, 0F, 4, 4, 8);
+      body6.setRotationPoint(0F, 16F, 33F);
+      body6.setTextureSize(64, 32);
+      body6.mirror = true;
+      setRotation(body6, 0F, 0F, 0F);
+      body5 = new ModelRenderer(this, 0, 0);
+      body5.addBox(0F, 0F, 0F, 4, 3, 6);
+      body5.setRotationPoint(-2F, 15F, 40F);
+      body5.setTextureSize(64, 32);
+      body5.mirror = true;
+      setRotation(body5, 0F, 0F, 0F);
+      head = new ModelRenderer(this, 0, 0);
+      head.addBox(-3F, -2F, -8F, 6, 6, 8);
+      head.setRotationPoint(0F, 14F, -16F);
+      head.setTextureSize(64, 32);
+      head.mirror = true;
+      setRotation(head, 0F, 0F, 0F);
   }
   
   public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
   {
     super.render(entity, f, f1, f2, f3, f4, f5);
     setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-    head.render(f5);
+    neck.render(f5);
     body1.render(f5);
     leftpectoralfin.render(f5);
     rightpectoralfin.render(f5);
-    jaw.render(f5);
     pectoralfinr.render(f5);
     body3.render(f5);
     body4.render(f5);
-    lefttail.render(f5);
-    righttail.render(f5);
+    tail.render(f5);
+    body6.render(f5);
+    body5.render(f5);
+    head.render(f5);
   }
   
   private void setRotation(ModelRenderer model, float x, float y, float z)
