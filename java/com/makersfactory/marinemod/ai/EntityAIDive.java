d1cc855b912adc74d0bb81197c56e331f9434c4f
Joe Wise <JosephWise93@gmail.com>
2014-12-01 17:01:31 -0800

    updated EntityAIDive. Borrowed a lot from EntityAISwimming.

--- java/com/makersfactory/marinemod/ai/EntityAIDive.java
+++ java/com/makersfactory/marinemod/ai/EntityAIDive.java
@@ -4,6 +4,7 @@ import com.makersfactory.marinemod.entity.EntityDolphin;
 import com.makersfactory.marinemod.entity.EntityElephantSeal;
 
 import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.world.biome.BiomeGenBase;
 
 public class EntityAIDive extends EntityAIBase {
 	
@@ -19,8 +20,49 @@ public class EntityAIDive extends EntityAIBase {
 	
 	@Override
 	public boolean shouldExecute() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+			if (this.dolphin.isInWater() && Math.random() > 0.1D) { // debug. TODO remove
+				return true;
+			} else {
+				return false;
+			}
+		}
+	
+	@Override
+	public void startExecuting() {
+		double targetX, targetY, targetZ; // where to head towards
+		boolean found_path = false; // could we find a path to swim to?
+		
+		// DEBUG
+		System.out.println("EntityAIDive startExecute()");
+
+		//Taken from Entity AI Swim
+
+//		Dolphin should always be in the water so we don't need to check
+//		if (!this.dolphin.getBiome().equals(BiomeGenBase.beach)) {
+//			System.out.println("EntityAISwim: entity is not in the beach biome");
+//			System.out.println("Current Biome: " + this.dolphin.getBiome());
+//			System.out.println("Beach Biome: " + BiomeGenBase.beach);
+//			// if you are out of the biome. move towards home (move 20% the distance from current position to spawn position).
+//			for (int try_cnt=1; try_cnt <= 5 && !found_path; ++try_cnt) {
+//				targetX = dolphin.nearByBeachX + ((dolphin.posX - dolphin.nearByBeachX) * .2) + ((Math.random() * 10.0) - 5.0);
+//				targetY = dolphin.nearByBeachY + ((dolphin.posY - dolphin.nearByBeachY) * .2) + ((Math.random() * 10.0) - 5.0);
+//				targetZ = dolphin.nearByBeachZ + ((dolphin.posZ - dolphin.nearByBeachZ) * .2)+ ((Math.random() * 10.0) - 5.0);
+//				this.dolphin.getNavigator().tryMoveToXYZ(targetX, targetY, targetZ, dolphin.swimSpeed);
+//				found_path = ! (this.dolphin.getNavigator().noPath());
+//				// DEBUG
+//				System.out.println("EntityAISwim startExecute(): noPath (" + targetX + "," + targetY + "," + targetZ +")?? try" + try_cnt + ": " + !found_path);
+//			}
+//		} else {
+			for (int try_cnt=1; try_cnt <= 5 && !found_path; ++try_cnt) {
+				targetX = dolphin.posX + ((Math.random() * 10.0) - 5.0); // TODO is [-5.0, 5.0) good?
+				targetY = dolphin.posY + ((Math.random() * 10.0) - 5.0); // TODO is [-5.0, 5.0) good?
+				targetZ = dolphin.posZ + ((Math.random() * 40.0) - 20.0); // TODO is [-20.0, 20.0) good?
+				this.dolphin.getNavigator().tryMoveToXYZ(targetX, targetY, targetZ, dolphin.swimSpeed);
+				found_path = ! (this.dolphin.getNavigator().noPath());
+				// DEBUG
+				System.out.println("EntityAISwim startExecute(): noPath (" + targetX + "," + targetY + "," + targetZ +")?? try" + try_cnt + ": " + !found_path);
+			}
+		}
+	
 
 }
--- java/com/makersfactory/marinemod/entity/EntityDolphin.java
+++ java/com/makersfactory/marinemod/entity/EntityDolphin.java
@@ -1,5 +1,7 @@
 package com.makersfactory.marinemod.entity;
 
+import com.makersfactory.marinemod.ai.EntityAIDive;
+
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIFollowParent;
 import net.minecraft.entity.ai.EntityAILookIdle;
@@ -15,16 +17,22 @@ import net.minecraft.init.Items;
 import net.minecraft.world.World;
 
 public class EntityDolphin extends EntityWaterMob {
+	
+	public double swimSpeed;
 
 	public EntityDolphin(World p_i1695_1_) {
 		super(p_i1695_1_);
+		
 		this.setSize(1.0F,1.0F);
-		this.tasks.addTask(0, new EntityAISwimming(this));
-		this.tasks.addTask(1, new EntityAIPanic(this, 1.4D));
-		this.tasks.addTask(2,  new EntityAITempt(this, 1.0D, Items.fish, false));
-		this.tasks.addTask(3, new EntityAIWander(this, 1.0D));
-        this.tasks.addTask(4, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
-        this.tasks.addTask(5, new EntityAILookIdle(this));
+		this.swimSpeed = 1.0D;
+		
+		this.tasks.addTask(0, new EntityAIDive(this));
+//		this.tasks.addTask(0, new EntityAISwimming(this));
+//		this.tasks.addTask(1, new EntityAIPanic(this, 1.4D));
+//		this.tasks.addTask(2,  new EntityAITempt(this, 1.0D, Items.fish, false));
+//		this.tasks.addTask(3, new EntityAIWander(this, 1.0D));
+//        this.tasks.addTask(4, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+//        this.tasks.addTask(5, new EntityAILookIdle(this));
 	}
 
 
